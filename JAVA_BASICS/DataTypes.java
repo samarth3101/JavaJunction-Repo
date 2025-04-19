41e931721ba673f5cf751c43911dac09419e34b7
[Samarth Patil] <143065657+samarth3101@users.noreply.github.com>
2025-04-19 13:15:25 +0530

    Mastering JAVA

---
+++ JAVA_BASICS/DataTypes.java
@@ -0,0 +1,29 @@
+package JAVA_BASICS;
+
+public class DataTypes {
+    public static void main(String [] args){
+        //here we are going to discuss data types in java
+
+        // we have two main types primitive and non-primitive
+
+        System.out.println("Here below are the data types which we often use in java programming...!");
+
+        //primitive
+        int price = 45000;
+        float age = 3.5f;
+        char grade = 'A';
+        boolean isAvailable = true;
+
+        // non primitive
+        String name = "SAMARTH";
+        int[] marks = {45,55,67,23,89};
+
+        System.out.println("Price = " + price);
+        System.out.println("Age = " + age);
+        System.out.println("Grade = " + grade);
+        System.out.println("Is available (boolean) = " + isAvailable);
+
+        System.out.println("My name is = " + name);
+        System.out.println("List of marks is = " + marks);
+    }
+}
